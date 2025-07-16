4f11473d4e23bfdfbe4d7a170164813a24204f4e
iconnectionbb <iconnectionbb@gmail.com>
2025-07-16 16:25:16 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,16 +1,19 @@
 import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import certifi
 
 # Load environment variables from .env
 load_dotenv()
 
-# Get the URI from environment
+# Recommended: Store this in .env
 mongo_uri = os.getenv("MONGO_URI")
 
-# Connect to MongoDB
-client = MongoClient(mongo_uri)
-db = client['cable_recharge']
+# Setup TLS with certifi
+ca = certifi.where()
+client = MongoClient(mongo_uri, tls=True, tlsCAFile=ca)
 
+# Connect to database and collections
+db = client['cable_recharge']
 recharges = db['recharges']
 users = db['users']
